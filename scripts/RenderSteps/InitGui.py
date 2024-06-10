edc017f286d23a0e9410b8fd1fa59a09d35b24cd
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-06-10 22:17:00 +0200

    werkend?

--- scripts/RenderSteps/InitGui.py
+++ scripts/RenderSteps/InitGui.py
@@ -5,13 +5,21 @@ def runStartupMacros(name):
     # Do not run when NoneWorkbench is activated because UI isn't yet completely there
     print(name)
     if name != "NoneWorkbench":
-
         # Run macro only once by disconnecting the signal at first call
         FreeCADGui.getMainWindow().workbenchActivated.disconnect(runStartupMacros)
         
         FreeCAD.openDocument('/tmp/mirte-frame/freecadFiles/layer.FCStd')
         FreeCAD.setActiveDocument("layer")
         print("done")
+        App.setActiveDocument("layer")
+        App.ActiveDocument=App.getDocument("layer")
+        Gui.ActiveDocument=Gui.getDocument("layer")
+        doc = FreeCAD.ActiveDocument
+        doc.recompute()
+        Gui.runCommand('Std_OrthographicCamera',1)
+        Gui.Selection.addSelection('layer','Body001')
+        print("klaar!!!")
+
         # Following 2 lines shall be duplicated for each macro to run
         # import MySuperMacro
         # MySuperMacro.run()
